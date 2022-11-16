ff3719670237358431a7b83b7e626da314fef08b
nikol <nikolablesic@gmail.com>
2022-11-16 20:48:21 +0100

    Target folder

--- tracking/src/main/java/transkop/tracking/TrackingApplication.java
+++ tracking/src/main/java/transkop/tracking/TrackingApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAutoConfiguration
 public class TrackingApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+1"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackingApplication.class, args);
